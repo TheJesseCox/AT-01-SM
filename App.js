624198b3ae04ea37810e7ce92c3a3866b3bb1a08
TheJesseCox <thejessecox1@gmail.com>
2020-08-31 18:21:33 -0300

    Subindo alterações

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import IconAwesome from 'react-native-vector-icons/FontAwesome5';
+import IconMaterial from 'react-native-vector-icons/MaterialIcons';
+import IconEntypo from 'react-native-vector-icons/Entypo';
+
+//IconAwesome.loadFont(); //acusa erro: default.loadfont undefined
+IconMaterial.loadFont();
+IconEntypo.loadFont();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.views}><IconAwesome name="user-alt" size={30}></IconAwesome><Text>Perfil</Text></View>
+      <View style={styles.views}><IconMaterial name="camera-alt" size={30}></IconMaterial><Text>Câmera</Text></View>
+      <View style={styles.views}><IconEntypo name="log-out" size={30}></IconEntypo><Text>Sair</Text></View>
     </View>
   );
 }
@@ -14,8 +21,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#DEDEDE',
     alignItems: 'center',
     justifyContent: 'center',
+    margin:0,
+  },
+  views:{
+    backgroundColor: '#FAFAFA',
+    width: 300,
+    height: 200,
+    margin: 15,
+    justifyContent: 'center',
+    alignItems:'center',
+    shadowOffset:{width:0,height:1},
+    shadowOpacity:1,
+    shadowRadius:4,
+    elevation: 5,
+    borderRadius: 5,
   },
 });
